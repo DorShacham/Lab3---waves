6666a77296c460c8703e736201b5834a2b15b1b7
Dor <dor.shacham@gmail.com>
2022-08-30 22:15:39 +0300

    macro2

---
+++ exp4 - micro/macro2.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 21:21:43 2022
+
+@author: dorsh
+"""
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.stats import linregress
+from scipy.optimize import curve_fit as cfit
+
+
+
+
+# helper function for plotting data and regression
+def one4all(xdata,ydata,yerr=0,xerr=0,mode="general function",f=None,xlabel="x",ylabel="y"):
+    fig = plt.figure(dpi=300)
+    plt.errorbar(xdata,ydata,yerr,xerr,"o",label="Data")
+
+    
+    if mode == "none":
+        fit= []
+        
+        
+    
+    elif mode == "linear":
+        fit = linregress(xdata,ydata)
+        f = lambda x,a,b: a*x+b
+        #fit_label = "Regression: y=" + str(fit.slope) + str("x+") + str(fit.intercept)
+        plt.plot(xdata,f(xdata,fit.slope,fit.intercept),"-.",label="Regression")
+    
+       
+        
+    elif mode == "0 intercept":
+        f = lambda x,a: a*x
+        fit = cfit(f,xdata,ydata)
+        plt.plot(xdata,f(xdata,*fit[0]),"-.",label="Regression")
+        
+    elif mode == "general function":
+        if f==None:
+            raise TypeError
+
+        fit = cfit(f,xdata,ydata)
+        plt.plot(xdata,f(xdata,*fit[0]),"-.",label="Regression")
+    
+    
+    else:
+        print("mode='",mode,"' is not definted!")
+        raise TypeError
+
+
+    plt.xlabel(xlabel, fontsize=14)
+    plt.ylabel(ylabel, fontsize=14)
+    plt.grid()
+    if mode != "none":
+        plt.legend()
+
+    plt.show()
+    
+    return (fig,fit)
+
+
+
+#%% 3 michelson
+
+
+x_node = [1,1] #m
+x_max = [1,1]
+x_err = 0
+
+V_node = [1,1]
+V_max = [1,1]
+Vf_err = 0
+
+
+x_node = np.array(x_node)
+x_max = np.array(x_max)
+V_node = np.array(V_node)
+V_max = np.array(V_max)
+
+wavelen_node = 2 * (np.abs(np.diff(x_node))).mean()
+wavelen_node_err = 2 * sqrt(2) * x_err / sqrt(len(x_node) - 1)
+print('wavelen node:',wavelen_node,"+-",wavelen_node_err,"m")
+
+wavelen_max = 2 * (np.abs(np.diff(x_max))).mean()
+wavelen_max_err = 2* sqrt(2) * x_err / sqrt(len(x_max) - 1)
+print('wavelen max:',wavelen_max,"+-",wavelen_max_err,"m")
+
+
+wavelen = np.array([wavelen_node,wavelen_max]).mean()
+wavelen_err = sqrt(wavelen_node_err**2+wavelen_max_err**2)
+print('wavelen:',wavelen,"+-",wavelen_err,"m")
+
+x = np.append(x_node,x_max)
+V = np.append(V_node,V_max)
+fig = plt.figure()
+one4all(x,V,V_err,x_err,"none",xlabel="x [m]",ylabel="V [V]")
+
+#%% 4 ferbri febro 
+
+d1 = 0
+d2 = 0
+
+x_node = [1,1] #m
+x_err = 0
+
+V_node = [1,1]
+Vf_err = 0
+
+
+x_node = np.array(x_node)
+V_node = np.array(V_node)
+
+wavelen_node = 2 * (np.abs(np.diff(x_node))).mean()
+wavelen_node_err = 2 * sqrt(2) * x_err / sqrt(len(x_node) - 1)
+print('wavelen 1:',wavelen_node,"+-",wavelen_node_err,"m")
+
+
+#### part 2 
+d1 = 0
+d2 = 0
+
+x_node = [1,1] #m
+x_err = 0
+
+V_node = [1,1]
+V_err = 0
+
+
+x_node = np.array(x_node)
+V_node = np.array(V_node)
+
+wavelen_node2 = 2 * (np.abs(np.diff(x_node))).mean()
+wavelen_node_err2 = 2 * sqrt(2) * x_err / sqrt(len(x_node) - 1)
+print('wavelen 2:',wavelen_node2,"+-",wavelen_node_err2,"m")
+
+
+wavelen = np.array([wavelen_node,wavelen_node2]).mean()
+wavelen_err = np.sqrt(wavelen_node_err**2+wavelen_node_err2**2)
+print('wavelen:',wavelen,"+-",wavelen_err,"m")
+
+#%% 5 Loid
+
