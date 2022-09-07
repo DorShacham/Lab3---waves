b61130666d411c1ceb072b0eaf23b3a43da66886
Dor <dor.shacham@gmail.com>
2022-09-07 15:12:20 +0300

    .

--- exp5 - light/light2.py
+++ exp5 - light/light2.py
@@ -77,10 +77,16 @@ def print_seciont(str):
 #%% init
 T_env = 24.2 + 273.15 # in Kelvin!!
 T_env_err = 1
-p_env  = 741 #torr
-P_env_err = 1
+T_env = ufloat(T_env,T_env_err)
+
+p_env  = 761 #torr
+p_env_err = 1
+p_env = ufloat(p_env,p_env_err)
+
 L = 24.8e-2 #m
 L_err = 1e-3 #m
+L = ufloat(L,L_err)
+
 wavelen0 = 532e-9 #m
 kb = scipy.constants.k # Boltzmann const in J/k
 
@@ -90,12 +96,12 @@ T_nom = 273.15 # K, 0 C
 
 #%% air
 print_seciont("Air")
-p = np.array([ 426, 489,537,605,670,737])-426 # need to notice units and do conversion to cmHg or other the other way around
+p = np.array([ 426, 489,537,605,670,737]) # need to notice units and do conversion to cmHg or other the other way around
 p_err = 1#np.array([0.1,0.1])
 F = np.arange(0,len(p)) * 10 # lines that passes over the screen
 
 fig,fit = one4all(p, F,0,p_err,"linear",None,"p [mmHg]","F")
-m = fit.slope
+m = ufloat(fit.slope,2*fit.stderr)
 alpha = m * 2 * kb * T_env * wavelen0 / L
 print("Alpha air is:",alpha)
 
@@ -105,7 +111,7 @@ Reg_print(fit)
 fig.savefig("fig/plot_air")
 
 #%% CO2
-p_env=761
+p_env= ufloat(761,1)
 print_seciont("CO2")
 p = np.array([30,57,92,99,103,108,129,133,151,275,343,433,474,516,553,589,632,688,740,760]) # need to notice units and do conversion to cmHg or other the other way around
 p_err = 1
@@ -113,7 +119,7 @@ F= np.array([0,2,8,9,10,11,16,17,24,39,52,65,76,87,98,108,119,129,142,147])
 #F = np.arange(0,len(p)) * 10 # lines that passes over the screen
 
 fig,fit = one4all(p, F,0,p_err,"linear",None,"p [mmHg]","F")
-m = fit.slope
+m = ufloat(fit.slope,2*fit.stderr)
 alpha = m * 2 * kb * T_env * wavelen0 / L
 print("Alpha CO2 is:",alpha)
 
@@ -131,7 +137,7 @@ p = np.array([400,502,558,586,615,651,752])[:-2] # need to notice units and do c
 #F_del=np.array([3,2,1,1,2])
 F=np.array([0,3,5,6,7,9])[:-1]
 
-p_env=762
+p_env= ufloat(762,1)
 #p = np.array([32,41,60,78,137,175,193,204,304,380,414,557,616,662,697]) # need to notice units and do conversion to cmHg or other the other way around
 F_del=np.array([])
 #F=np.array([0,1,3,5,8,10,11,12,22,25,27,32,34,35,36])
@@ -139,7 +145,7 @@ p_err = 1
 #F = np.arange(0,len(p)) * 10 # lines that passes over the screen
 
 fig,fit = one4all(p, F,0,p_err,"linear",None,"p [mmHg]","F")
-m = fit.slope
+m = ufloat(fit.slope,2*fit.stderr)
 alpha = m * 2 * kb * T_env * wavelen0 / L
 print("Alpha He is:",alpha)
 
@@ -150,7 +156,7 @@ fig.savefig("fig/plot_He")
 
 #%% Mixture of CO2 and He
 print_seciont("Mixture of CO2 and He")
-p_env=769
+p_env= ufloat(769,1)
 '''
 first measurments
 p = np.array([29,86,159,232,258]) # need to notice units and do conversion to cmHg or other the other way around
@@ -170,7 +176,7 @@ F= np.array([0,6,16,26,36,46,57,68])
 
 fig,fit = one4all(p, F,0,p_err,"linear",None,"p [mmHg]","F")
 Reg_print(fit)
-m = fit.slope
+m = ufloat(fit.slope,2*fit.stderr)
 alpha = m * 2 * kb * T_env * wavelen0 / L
 print("Alpha mixture is:",alpha)
 
